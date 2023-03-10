36220bc0bfd7f10257777afabb7614c3de55cdf9
cokwa <rhrh1363@gmail.com>
2023-03-10 18:27:36 +0900

    change to a top-down approach

--- bit_htm_v2.py
+++ bit_htm_v2.py
@@ -10,6 +10,45 @@ except:
         out = np.concatenate([out, np.zeros(max(minLength - len(out), 0), dtype=out.dtype)])
         return out
 
+def construct_block_mapping(block_lengths):
+    total_block_length = block_lengths.sum()
+    nonempty_blocks, = np.nonzero(block_lengths)
+    block_borders = block_lengths.cumsum() - block_lengths
+    row_index = np.zeros(total_block_length, dtype=np.int32)
+    row_index[block_borders[nonempty_blocks]] = np.arange(len(block_lengths))[nonempty_blocks]
+    row_index = np.maximum.accumulate(row_index)
+    col_index = np.arange(total_block_length, dtype=np.int32)
+    col_index -= block_borders[row_index]
+    return (row_index, col_index)
+
+def nonzero_bounded_2d(value, bounds, offset=None, lengths=None):
+    if offset is None:
+        if lengths is None:
+            lengths = (value != 0).sum(axis=1)
+        _, offset = construct_block_mapping(lengths)
+    index = np.nonzero(value)
+    if type(bounds) == np.ndarray:
+        bounds = bounds[index[0]]
+    bounded = np.nonzero(offset < bounds)
+    return (index[0][bounded], index[1][bounded])
+
+# TODO TODO TODO: account for invalid entries in added arrays
+def replace_free(arrays, added_arrays, index, free, added_lengths=None):
+    assert len(index.shape) == 1 and len(free.shape) == 2
+    if added_lengths is None:
+        added_lengths = added_array.shape[1]
+    free_count = free.sum(axis=1)
+    resolved_lengths = np.minimum(free_count, added_lengths)
+    free_index = nonzero_bounded_2d(free, resolved_lengths, lengths=free_count)
+    array_index = (index[free_index[0]], free_index[1])
+    added_array_index = construct_block_mapping(resolved_lengths)
+    for array, added_array in zip(arrays, added_arrays):
+        if type(added_array) != np.ndarray:
+            array[array_index] = added_array
+            continue
+        array[array_index] = added_array[added_array_index]
+    return resolved_lengths
+
 
 class DynamicArray2D:
     def __init__(self, dtype, size=(0, 0), capacities=None, exponential_growths=(True, True), on_grow=None):
@@ -83,16 +122,18 @@ class DenseProjection:
 
         self.permanence = np.random.randn(output_dim, input_dim) * permanence_std + permanence_mean
 
-    def process(self, input):
+    def process(self, dense_input):
         weight = self.permanence >= self.permanence_threshold
-        overlaps = (weight & input).sum(axis=1)
+        overlaps = (weight & dense_input).sum(axis=1)
         return overlaps
 
-    def update(self, input, target):
-        self.permanence[target] += input * (self.permanence_increment + self.permanence_decrement) - self.permanence_decrement
+    def update(self, dense_input, learning_output):
+        self.permanence[learning_output] += dense_input * (self.permanence_increment + self.permanence_decrement) - self.permanence_decrement
 
 
 class SparseProjection:
+    projection_invalid_flag = 0x1 << 31
+
     def __init__(
         self, input_dim, output_dim=0,
         projection_exponential_growth=False, output_exponential_growth=True
@@ -101,43 +142,125 @@ class SparseProjection:
         self.output_dim = output_dim
 
         def on_projection_grow(new_values, new_size, new_capacities):
-            assert np.log2(self.input_dim * new_capacities) <= 31
+            assert np.log2(self.input_dim * new_capacities[1]) <= 31
             macro, micro = divmod(self.backprojection[:], self.projection.capacities[1])
             self.backprojection[:] = macro * new_capacities[1] + micro
 
         self.projection = DynamicArray2D(np.int32, size=(self.input_dim, 0), exponential_growths=(False, projection_exponential_growth), on_grow=on_projection_grow)
-        self.projection_weight = DynamicArray2D(np.bool_, size=(self.input_dim, 0), exponential_growths=(False, projection_exponential_growth))
-        
         self.backprojection = DynamicArray2D(np.int32, size=(self.output_dim, 0), exponential_growths=[output_exponential_growth, projection_exponential_growth])
         self.backprojection_permanence = DynamicArray2D(np.float32, size=(self.output_dim, 0), exponential_growths=[output_exponential_growth, projection_exponential_growth])
+
+    def get_pure_projection(self, flaggable_projection):
+        return flaggable_projection & (~SparseProjection.projection_invalid_flag)
+
+    def get_projection_validity(self, flaggable_projection):
+        return (flaggable_projection & SparseProjection.projection_invalid_flag) == 0x0
+
+    def get_projection_invalidity(self, flaggable_projection):
+        return (flaggable_projection & SparseProjection.projection_invalid_flag) != 0x0
+
+    def decompose_flaggable_projection(self, flaggable_projection):
+        return self.get_pure_projection(flaggable_projection), self.get_projection_validity(flaggable_projection)
+
+    def add_output_dim(self, added_output_dim):
+        assert added_output_dim >= 0
+        self.output_dim += added_output_dim
+        assert np.log2(self.output_dim) <= 31
+        self.backprojection.add_rows(np.random.randint(0, self.input_dim * self.projection.capacities[1], (added_output_dim, self.backprojection.size[1]), dtype=self.backprojection.dtype))
+        self.backprojection_permanence.add_rows(np.full((added_output_dim, 1), -1.0, dtype=self.backprojection_permanence.dtype))
+
+    def update_permanence(self, input_activation, learning_output, permanence_chage_active, permanence_change_inactive, return_projection_info=False):
+        learning_backprojection_macro, learning_backprojection_micro = divmod(self.backprojection[learning_output], self.projection.capacities[1])
+        learning_backprojection_permanence = self.backprojection_permanence[learning_output]
+        learning_backprojection_valid = learning_backprojection_permanence > 0.0
+        backprojected_activation = input_activation[learning_backprojection_macro]
+        naive_permanence_change = backprojected_activation * (permanence_chage_active - permanence_change_inactive) + permanence_change_inactive
+        updated_permanence = learning_backprojection_permanence + learning_backprojection_valid * naive_permanence_change
+        self.backprojection_permanence[learning_output] = updated_permanence
+        self.projection[
+            learning_backprojection_macro,
+            learning_backprojection_micro
+        ] &= ((updated_permanence <= 0.0) * SparseProjection.projection_invalid_flag) | (~SparseProjection.projection_invalid_flag)
+
+        if return_projection_info:
+            return learning_backprojection_macro, learning_backprojection_micro, learning_backprojection_permanence, learning_backprojection_valid
+
+    def add_projections(
+        self, input_activation, learning_output, winner_input, permanence_initial, min_active_projections,
+        learning_backprojection_macro=None, learning_backprojection_valid=None
+    ):
+        assert permanence_initial >= 0.0
+        if learning_backprojection_macro is None:
+            learning_backprojection_macro = self.backprojection[learning_output] // self.projection.capacities[1]
+        if learning_backprojection_valid is None:
+            learning_backprojection_valid = self.backprojection_permanence[learning_output] > 0.0
+
+        whole_input_to_winner = np.random.randint(0, len(winner_input), self.projection.size[0], dtype=np.int32) | SparseProjection.projection_invalid_flag
+        whole_input_to_winner[winner_input] = np.arange(len(winner_input))
+        backprojection_winner_macro, backprojection_winner_macro_valid = self.decompose_flaggable_projection(whole_input_to_winner[learning_backprojection_macro])
+        candidate_priority = np.random.rand(len(learning_output), len(winner_input)).astype(np.float32)
+        candidate_priority += bincount(
+            np.repeat(np.arange(len(learning_output)), self.backprojection.size[1]) * len(winner_input) + backprojection_winner_macro.flatten(),
+            weights=learning_backprojection_valid.flatten() & backprojection_winner_macro.flatten(),
+            minLength=len(learning_output) * len(winner_input)
+        ).reshape(candidate_priority.shape)
+        
+        added_projections = np.clip(min_active_projections - input_activation[learning_backprojection_macro].sum(axis=1), 0, min(min_active_projections, len(winner_input)))
+        max_added_projections = added_projections.max()
+        priority_argsort = np.argpartition(candidate_priority, min(max_added_projections, candidate_priority.shape[1] - 1), axis=1)[:, :max_added_projections]
+
+        candidate = winner_input[priority_argsort]
+        already_connected = np.take_along_axis(candidate_priority, priority_argsort, axis=1) < 1.0
+        candidate_prioritized = np.tile(np.arange(max_added_projections), (len(learning_output), 1)) < np.expand_dims(added_projections, 1)
+        candidate_valid = already_connected & candidate_prioritized
+
+        resolved_backprojections = replace_free(
+            [self.backprojection, self.backprojection_permanence],
+            [candidate_prioritized, permanence_initial],
+            learning_output, ~learning_backprojection_valid, added_lengths=added_projections
+        )
+        # remaining_backprojections = added_projections - remaining_backprojections
+        # max_remaining_backprojections = remaining_backprojections.max()
+        # if max_remaining_backprojections > 0:
+        #     added_backprojection = np.random.randint(0, self.input_dim * self.projection.capacities[1], (self.backprojection.size[0], max_remaining_backprojections), dtype=np.int32)
+        #     added_backprojection_permanence = np.full((self.backprojection.size[0], max_remaining_backprojections), -1.0, dtype=np.float32)
+        #     added_backprojection[learning_output]
+        #     self.backprojection.add_cols(added_backprojection)
+        #     self.backprojection_permanence.add_cols(added_backprojection_permanence)
+        
+        
+        # replace_free(
+        #     [self.projection],
+        #     [],
+        #     winner_input, self.get_projection_invalidity(self.projection[winner_input]), added_lengths=None
+        # )
 
     def process(self, active_input=None, dense_input=None):
         assert (active_input is None) ^ (dense_input is None)
         if dense_input is not None:
             active_input = np.nonzero(dense_input)
-        projected = bincount(self.projection[active_input], weights=self.projection_weight[active_input], minLength=self.output_dim)        
-        assert projected.shape[0] != self.output_dim
+
+        active_projection, active_weight = self.decompose_flaggable_projection(self.projection[active_input].flatten())
+        projected = bincount(active_projection, weights=active_weight, minLength=self.output_dim)        
+        
+        assert projected.shape[0] == self.output_dim
         return projected
 
     def update(
-        self, input_activation, learning_output, winner_input, winner_output, added_outputs=0,
-        permanence_initial=0.01, permanence_increase=0.3, permanence_decrease=0.05
+        self, input_activation, learning_output, winner_input=None, added_output_dim=0,
+        permanence_initial=0.01, permanence_chage_active=0.3, permanence_change_inactive=-0.05,
+        min_active_projections=20
     ):
-        assert added_outputs >= 0
-        assert permanence_initial >= 0.0
-
-        learning_backprojection_macro, learning_backprojection_micro = divmod(self.backprojection[learning_output], self.projection.capacities[1])
-        learning_backprojection_permanence = self.backprojection_permanence[learning_output]
-        learning_backprojection_valid = learning_backprojection_permanence > 0.0
-        backprojected_activation = input_activation[learning_backprojection_macro]
-        updated_permanence = learning_backprojection_permanence + learning_backprojection_valid * (backprojected_activation * (permanence_increase + permanence_decrease) - permanence_decrease)
-        self.backprojection_permanence[learning_output] = updated_permanence
-        self.projection_weight[learning_backprojection_macro, learning_backprojection_micro] = updated_permanence > 0.0
-
-        if added_outputs > 0:
-            self.output_dim += added_outputs
-            self.backprojection.add_rows(np.random.randint(0, self.input_dim * self.projection.capacities[1], (added_outputs, self.backprojection.size[1]), dtype=self.backprojection.dtype))
-            self.backprojection_permanence.add_rows(np.full((added_outputs, 1), -1.0, dtype=self.backprojection_permanence.dtype))
+        if added_output_dim > 0:
+            self.add_output_dim(added_output_dim)
+        learning_backprojection_macro, _, _, learning_backprojection_valid = self.update_permanence(
+            input_activation, learning_output, permanence_chage_active, permanence_change_inactive, return_projection_info=True
+        )
+        if winner_input is not None:
+            self.add_projections(
+                input_activation, learning_output, winner_input, permanence_initial, min_active_projections,
+                learning_backprojection_macro=learning_backprojection_macro, learning_backprojection_valid=learning_backprojection_valid
+            )
 
 
 class ExponentialBoosting:
@@ -175,14 +298,17 @@ class SpatialPooler:
             self.boosted_overlaps = boosted_overlaps
             self.active_column = active_column
 
-    def __init__(self, input_dim, column_dim, active_columns):
+    def __init__(
+        self, input_dim, column_dim, active_columns,
+        proximal_projection=None, boosting=None, inhibition=None
+    ):
         self.input_dim = input_dim
         self.column_dim = column_dim
         self.active_columns = active_columns
 
-        self.proximal_projection = DenseProjection(input_dim, column_dim)
-        self.boosting = ExponentialBoosting(column_dim, active_columns)
-        self.inhibition = GlobalInhibition(active_columns)
+        self.proximal_projection = proximal_projection or DenseProjection(input_dim, column_dim)
+        self.boosting = boosting or ExponentialBoosting(column_dim, active_columns)
+        self.inhibition = inhibition or GlobalInhibition(active_columns)
 
     def process(self, input, learning=True):
         overlaps = self.proximal_projection.process(input)
@@ -237,12 +363,14 @@ class TemporalMemory:
 
 
 class HierarchicalTemporalMemory:
-    def __init__(self, input_dim, column_dim, cell_dim, active_columns=None):
-        if active_columns is None:
-            active_columns = round(column_dim * 0.02)
+    def __init__(
+        self, input_dim, column_dim, cell_dim, active_columns=None,
+        spatial_pooler=None, temporal_memory=None
+    ):
+        active_columns = active_columns or round(column_dim * 0.02)
 
-        self.spatial_pooler = SpatialPooler(input_dim, column_dim, active_columns)
-        self.temporal_memory = TemporalMemory(column_dim, cell_dim)
+        self.spatial_pooler = spatial_pooler or SpatialPooler(input_dim, column_dim, active_columns)
+        self.temporal_memory = temporal_memory or TemporalMemory(column_dim, cell_dim)
 
     def process(self, input, learning=True):
         sp_state = self.spatial_pooler.process(input, learning=learning)
